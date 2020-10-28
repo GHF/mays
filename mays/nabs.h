a3ecc2e9004a2dbad6d2470ce2743b0ca8c3d43e
Xo Wang <xo@geekshavefeelings.com>
2020-10-28 00:16:30 -0700

    ✨ Add Nabs
    Also unflatten CMakeLists structure to recursively include
    subdirectories

---
+++ mays/nabs.h
@@ -0,0 +1,35 @@
+// (C) Copyright 2014 Xo Wang <xo@geekshavefeelings.com>
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef MAYS_NABS_H
+#define MAYS_NABS_H
+
+#include <type_traits>
+
+#include "internal/type_traits.h"
+
+namespace mays {
+
+// Takes the negation of the absolute value of an integer. Unlike abs, this is defined for all input
+// values, including INT_MIN. See ISO/IEC 14882:2017 § 29.9.2 [c.math.abs] ¶ 2 which defers to ISO C
+// § 7.20.6.1 ¶ 2 and footnote for the description of abs/labs/llabs behavior with INT_MIN in two's
+// complement.
+template <typename T>
+constexpr T Nabs(T i) {
+  static_assert(std::is_integral_v<T> && std::is_signed_v<T>, "Valid only for signed integers");
+  // Check if signed right shift sign extends (i.e. arithmetic right shift).
+  if constexpr (::mays::internal::has_arithmetic_shift_v<T>) {
+    constexpr int num_bits = sizeof(T) * 8;
+    // Splat sign bit into all bit positions. |sign| takes a value of either 0 or -1.
+    const T sign = i >> (num_bits - 1);
+    // If |i| is positive (|sign| = 0), |i| is unchanged by xor and subtracted from 0.
+    // Otherwise if |i| is negative (|sign| = -1), this is equivalent to |-(1 + ~i)| or just |i|.
+    return sign - (i ^ sign);
+  } else {
+    return i <= 0 ? i : -i;
+  }
+}
+
+}  // namespace mays
+
+#endif  // MAYS_NABS_H
---
+++ mays/nabs_test.cc
@@ -0,0 +1,24 @@
+// (C) Copyright 2019 Xo Wang <xo@geekshavefeelings.com>
+// SPDX-License-Identifier: Apache-2.0
+
+#include "nabs.h"
+
+#include <catch2/catch.hpp>
+
+namespace mays {
+namespace {
+
+TEST_CASE("Take negative absolute value", "[nabs]") {
+  static_assert(0 == Nabs(0));
+  static_assert(-1 == Nabs(1));
+  static_assert(-2 == Nabs(2));
+  static_assert(-3 == Nabs(3));
+  static_assert(-1 == Nabs(-1));
+  static_assert(-2 == Nabs(-2));
+  static_assert(-3 == Nabs(-3));
+  static_assert(std::numeric_limits<int>::min() == Nabs(std::numeric_limits<int>::min()));
+  static_assert(std::numeric_limits<int>::min() + 1 == Nabs(std::numeric_limits<int>::max()));
+}
+
+}  // namespace
+}  // namespace mays
