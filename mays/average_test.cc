d11cfd2619c77cf392db635b9faa04c69440baf3
Xo Wang <xo@geekshavefeelings.com>
2020-10-29 23:50:31 -0700

    🥽 Extract magic numbers of Average's test cases 🌈

--- mays/average_test.cc
+++ mays/average_test.cc
@@ -1,4 +1,4 @@
-// (C) Copyright 2019 Xo Wang <xo@geekshavefeelings.com>
+// (C) Copyright 2020 Xo Wang <xo@geekshavefeelings.com>
 // SPDX-License-Identifier: Apache-2.0
 
 #include "average.h"
@@ -9,17 +9,23 @@ namespace mays {
 namespace {
 
 TEST_CASE("Average(a, b) = Average(b, a)", "[average]") {
-  static_assert(4 == Average(2, 7));
-  static_assert(4 == Average(7, 2));
-  static_assert(-4 == Average(-2, -7));
-  static_assert(-4 == Average(-7, -2));
+  constexpr int a = 2;
+  constexpr int b = 7;
+  constexpr int c = 4;
+  static_assert(c == Average(a, b));
+  static_assert(c == Average(b, a));
+  static_assert(-c == Average(-a, -b));
+  static_assert(-c == Average(-b, -a));
 }
 
 TEST_CASE("Average(a + 1, b + 1) = Average(a, b) + 1", "[average]") {
-  static_assert(3 == Average(2, 4));
-  static_assert(4 == Average(3, 5));
-  static_assert(-3 == Average(-2, -4));
-  static_assert(-4 == Average(-3, -5));
+  constexpr int a = 2;
+  constexpr int b = 4;
+  constexpr int c = 3;
+  static_assert(c == Average(a, b));
+  static_assert(c + 1 == Average(a + 1, b + 1));
+  static_assert(-c == Average(-a, -b));
+  static_assert(-c - 1 == Average(-a - 1, -b - 1));
 }
 
 TEST_CASE("Average(a, -b) = -Average(-a, b)", "[average]") {
@@ -31,24 +37,45 @@ TEST_CASE("Average(a, -b) = -Average(-a, b)", "[average]") {
   static_assert(-1 == Average(0, -3));
 }
 
-TEST_CASE("Average doesn't overflow for signed int", "[average]") {
-  // ...if a + b overflows.
-  static_assert(0x5000'0001 == Average(0x5000'0000, 0x5000'0002));
-  static_assert(-2'000'000'001 == Average(-2'000'000'000, -2'000'000'002));
+TEST_CASE("Average doesn't overflow for int", "[average]") {
+  SECTION("...if a + b overflows") {
+    constexpr int a = 0x5000'0000;
+    constexpr int b = 0x5000'0002;
+    constexpr int c = 0x5000'0001;
+    static_assert(c == Average(a, b));
+  }
 
-  // ...if a - b overflows.
-  static_assert(-1 == Average(2'000'000'000, -2'000'000'002));
+  SECTION("...if a + b underflows") {
+    constexpr int a = -2'000'000'002;
+    constexpr int b = -2'000'000'000;
+    constexpr int c = -2'000'000'001;
+    static_assert(c == Average(a, b));
+  }
 
-  // ...if b - a overflows.
-  static_assert(1 == Average(-2'000'000'000, 2'000'000'002));
+  SECTION("...if a - b overflows") {
+    constexpr int a = 2'000'000'000;
+    constexpr int b = -2'000'000'002;
+    constexpr int c = -1;
+    static_assert(c == Average(a, b));
+  }
+
+  SECTION("...if b - a overflows") {
+    constexpr int a = -2'000'000'000;
+    constexpr int b = 2'000'000'002;
+    constexpr int c = 1;
+    static_assert(c == Average(a, b));
+  }
 }
 
 TEST_CASE("Average works with unsigned int", "[average]") {
   // ...if a + b overflows.
-  static_assert(0x9000'0001U == Average(0x9000'0000U, 0x9000'0002U));
+  constexpr unsigned int a = 0x9000'0000;
+  constexpr unsigned int b = 0x9000'0002;
+  constexpr unsigned int c = 0x9000'0001;
+  static_assert(c == Average(a, b));
 
   // ...if a > b.
-  static_assert(0x9000'0001U == Average(0x9000'0002U, 0x9000'0000U));
+  static_assert(c == Average(b, a));
 }
 
 }  // namespace
