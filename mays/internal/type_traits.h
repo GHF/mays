f66de625237ef6fd3483f826963f1c8f1b0d9994
Xo Wang <xo@geekshavefeelings.com>
2020-11-01 23:08:47 -0800

    ✨ Add is_convertible_without_narrowing type trait
    Almost a polyfill because it's a shoo-in for C++2b, but it's not there
    yet.

--- mays/internal/type_traits.h
+++ mays/internal/type_traits.h
@@ -6,7 +6,27 @@
 
 #include <type_traits>
 
+#include "polyfill.h"
+
 namespace mays::internal {
+
+// True if |From| is implicitly convertible to |To| without going through a narrowing conversion.
+// Will likely be included in C++2b through WG21 P0870 (see
+// https://github.com/cplusplus/papers/issues/724).
+template <typename From, typename To, typename Enable = void>
+struct is_convertible_without_narrowing : std::false_type {};
+
+// Implement "construct array of From" technique from P0870R4 with SFINAE instead of requires.
+template <typename From, typename To>
+struct is_convertible_without_narrowing<
+    From,
+    To,
+    // NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+    std::void_t<decltype(type_identity_t<To[]>{std::declval<From>()})>> : std::true_type {};
+
+template <typename From, typename To>
+constexpr bool is_convertible_without_narrowing_v =
+    is_convertible_without_narrowing<From, To>::value;
 
 // True if T sign-extends when right-shifted (two's complement representation is assumed).
 template <typename T, typename = std::enable_if_t<std::is_integral_v<T>>>
