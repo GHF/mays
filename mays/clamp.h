faefe24c4d595eb80099d0eb53a992947f720870
Xo Wang <xo@geekshavefeelings.com>
2020-11-02 02:23:49 -0800

    ✨ Add Clamp

---
+++ mays/clamp.h
@@ -0,0 +1,81 @@
+// (C) Copyright 2014 Xo Wang <xo@geekshavefeelings.com>
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef MAYS_CLAMP_H
+#define MAYS_CLAMP_H
+
+#include <limits>
+#include <type_traits>
+
+#include "internal/check.h"
+#include "internal/type_traits.h"
+
+namespace mays {
+
+// Clamps a value to a range. The lower bound must be less than or equal to the uppper bound.
+//
+// Example:
+//   auto clamped_value = Clamp(int{value}, 16, 235);  // 16 ≤ |clamped_value| ≤ 235
+//
+// For values of integer types, the bounds must be either implicitly convertible to the value's type
+// without a narrowing conversion (e.g. signed -> unsigned) or otherwise be a non-impossible bound
+// (e.g. a lower bound greater than the maximum value representable).
+//
+// Example:
+//   auto clamped_value = Clamp(int8_t{value}, -1024, 1024);  // |clamped_value| = |value|
+//
+// Negative clamp lower bounds are permitted even for unsigned values to be clamped.
+//
+// Example:
+//   auto clamped_value = Clamp(uint8_t{value}, -1024, 200);  // |clamped_value| ≤ 200
+//
+// For values of other types, the types of the bounds are deduced from that of the value. This means
+// that, for example, |float| values will have |float| bounds. C++ permits implicit conversions from
+// integers to |float| and more precise floating-point types to |float|. This behavior is allowed
+// here to let |float| bounds parameters accept integer literals and |double| literals.
+//
+// Example:
+//   auto clamped_value = Clamp(float{value}, 0, +.01);  // std requires clamp(value, 0.f, +.01f)
+//
+// Modern compilers will warn about the above implicit conversions if the literals' values can't be
+// exactly represented with |float|.
+template <typename T, typename L, typename H, typename = std::enable_if_t<std::is_integral_v<T>>>
+[[nodiscard]] constexpr T Clamp(T v, L lo, H hi) {
+  if constexpr (!internal::is_convertible_without_narrowing_v<L, T>) {
+    static_assert(std::is_integral_v<L>, "Integer Clamp's lower bound must be an integer.");
+    MAYS_CHECK(internal::cmp_less_equal(
+        lo, std::numeric_limits<T>::max()));  // NOLINT(bugprone-assert-side-effect)
+  }
+  if constexpr (!internal::is_convertible_without_narrowing_v<H, T>) {
+    static_assert(std::is_integral_v<H>, "Integer Clamp's upper bound must be an integer.");
+    MAYS_CHECK(internal::cmp_greater_equal(
+        hi, std::numeric_limits<T>::min()));  // NOLINT(bugprone-assert-side-effect)
+  }
+  MAYS_CHECK(internal::cmp_less_equal(lo, hi));  // NOLINT(bugprone-assert-side-effect)
+  const T lower_bounded = internal::cmp_greater_equal(v, lo) ? v : T(lo);
+  const T both_bounded = internal::cmp_less_equal(lower_bounded, hi) ? lower_bounded : T(hi);
+  return both_bounded;
+}
+
+template <typename T, typename = std::enable_if_t<!std::is_integral_v<T>>>
+[[nodiscard]] constexpr T Clamp(T v,
+                                internal::type_identity_t<T> lo,
+                                internal::type_identity_t<T> hi) {
+  if constexpr (std::is_floating_point_v<T>) {
+    // Check for NaNs without using |std::isnan|, which is not constexpr
+    MAYS_CHECK(lo == lo);
+    MAYS_CHECK(hi == hi);
+    if (v != v) {  // NOLINT(misc-redundant-expression)
+      return v;
+    }
+  }
+
+  MAYS_CHECK(lo <= hi);
+  const T lower_bounded = v >= lo ? v : lo;
+  const T both_bounded = lower_bounded <= hi ? lower_bounded : hi;
+  return both_bounded;
+}
+
+}  // namespace mays
+
+#endif  // MAYS_CLAMP_H
---
+++ mays/clamp_test.cc
@@ -0,0 +1,67 @@
+// (C) Copyright 2020 Xo Wang <xo@geekshavefeelings.com>
+// SPDX-License-Identifier: Apache-2.0
+
+#include "clamp.h"
+
+#include <catch2/catch.hpp>
+
+namespace mays {
+namespace {
+
+TEST_CASE("Clamp integer value to range", "[clamp]") {
+  constexpr auto kLower = 1;
+  constexpr auto kUpper = 10;
+  static_assert(5 == Clamp(5, kLower, kUpper));
+  static_assert(kLower == Clamp(kLower, kLower, kUpper));
+  static_assert(kLower == Clamp(kLower - 1, kLower, kUpper));
+  static_assert(kUpper == Clamp(kUpper, kLower, kUpper));
+  static_assert(kUpper == Clamp(kUpper + 1, kLower, kUpper));
+
+  // |int| bounds for a |uint8_t| value
+  static_assert(4 == Clamp(uint8_t{4}, kLower, kUpper));
+  static_assert(4 == Clamp(uint8_t{4}, -kLower, kUpper));
+}
+
+TEST_CASE("Clamp floating point value to range", "[clamp]") {
+  constexpr auto kLower = 1.;
+  constexpr auto kUpper = 10.;
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+  static_assert(5. == Clamp(5., kLower, kUpper));
+  static_assert(kLower == Clamp(kLower, kLower, kUpper));
+  static_assert(kLower == Clamp(kLower - 1, kLower, kUpper));
+  static_assert(kUpper == Clamp(kUpper, kLower, kUpper));
+  static_assert(kUpper == Clamp(kUpper + 1, kLower, kUpper));
+
+  // Sign of zero is preserved from input
+  static_assert(+0. == Clamp(+0., -0., +0.));
+  static_assert(-0. == Clamp(-0., -0., +0.));
+
+  // +0 as a lower bound still passes through -0 and vice versa
+  static_assert(-0. == Clamp(-0., +0., +1.));
+  static_assert(+0. == Clamp(+0., -1., -0.));
+
+  // |float| bounds for a |double| value
+  // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
+  static_assert(1. == Clamp(2., -1.f, 1.f));
+
+  // Clamp infinity and clamp to infinities
+  if constexpr (std::numeric_limits<float>::has_infinity) {
+    constexpr auto kNegativeInf = -std::numeric_limits<float>::infinity();
+    constexpr auto kPositiveInf = std::numeric_limits<float>::infinity();
+    static_assert(kLower == Clamp(kNegativeInf, kLower, kUpper));
+    static_assert(kUpper == Clamp(kPositiveInf, kLower, kUpper));
+    static_assert(0.f == Clamp(0.f, kNegativeInf, kPositiveInf));
+    static_assert(kNegativeInf == Clamp(kNegativeInf, kNegativeInf, kPositiveInf));
+    static_assert(kPositiveInf == Clamp(kPositiveInf, kNegativeInf, kPositiveInf));
+  }
+
+  // Clamping NaN returns the NaN
+  if constexpr (std::numeric_limits<float>::has_quiet_NaN) {
+    auto clamped_nan = Clamp(std::numeric_limits<float>::quiet_NaN(), kLower, kUpper);
+    CAPTURE(clamped_nan);
+    CHECK(FP_NAN == std::fpclassify(clamped_nan));
+  }
+}
+
+}  // namespace
+}  // namespace mays
