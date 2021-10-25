627d9a2c02c83af5d6f080d96f5359688ead7969
Xo Wang <xo@geekshavefeelings.com>
2021-10-25 16:11:58 -0700

    ✨ Add RangeMap linear value mapper

---
+++ mays/range_map.h
@@ -0,0 +1,144 @@
+// (C) Copyright 2014 Xo Wang <xo@geekshavefeelings.com>
+// SPDX-License-Identifier: Apache-2.0
+// vim: et:sw=2:ts=2:tw=100
+
+#ifndef MAYS_RANGE_MAP_H
+#define MAYS_RANGE_MAP_H
+
+#include <algorithm>
+#include <numeric>
+#include <tuple>
+#include <type_traits>
+
+#include "average.h"
+#include "clamp.h"
+#include "internal/check.h"
+#include "internal/type_traits.h"
+#include "nabs.h"
+#include "negate_if.h"
+#include "scale.h"
+
+namespace mays {
+
+// Maps one range of integer values linearly to another, with deadband. Care should be taken to
+// choose ranges and integer types that scale without overflow.
+//
+// Example:
+//   constexpr RangeMap map(/*in_range=*/{int8_t{-127}, int8_t{127}},
+//                          /*out_ends*/{1000, 2000},
+//                          /*deadband=*/int8_t{10});
+//   const int kMapped = map.Map(11);  // kMapped = 1505
+template <typename In, typename Out>
+class RangeMap final {
+ public:
+  // Construct a mapping for an input range spanning from |in_range| into an output range spanning
+  // from |out_a| to |out_b| (both ranges are inclusive).
+  //
+  // The input range must span strictly more than twice |deadband| and the limits must be in sorted
+  // order. |deadband| must be non-negative. The output range limits may be decreasing in order.
+  // Regardless, the low end of the input range will map to |out_a| and the high end will map to
+  // |out_b|.
+  //
+  // Input range is a tuple in order to differentiate the input range from deadband and output
+  // range when reading an instantiation.
+  // Example:
+  //   RangeMap map({-100, 100}, {-333, 333}, 5);
+  // Compared to:
+  //   RangeMap map(-100, 100, -333, 333, 5);  // Less readable
+  constexpr RangeMap(std::tuple<In, In> in_range, std::tuple<Out, Out> out_ends, In deadband = 0)
+      : in_lo_(std::get<0>(in_range)),
+        in_hi_(std::get<1>(in_range)),
+        deadband_(deadband),
+        in_midpoint_(Average(in_lo_, in_hi_)),
+        out_range_(std::minmax(std::get<0>(out_ends), std::get<1>(out_ends))),
+        out_midpoint_(Average(std::get<0>(out_ends), std::get<1>(out_ends))),
+        in_to_out_scaler_(ComputeScaler(std::get<0>(out_ends) > std::get<1>(out_ends))),
+        requires_out_clamp_(requires_out_clamp()) {}
+
+  // Linearly map |value| from the input range to output range. Values that are within |deadband|
+  // distance to the midpoint of the input range will be mapped as if they are midpoint. If |value|
+  // is outside of the input range, it will be clamped to the nearest input range limit.
+  [[nodiscard]] constexpr Out Map(In value) const {
+    // Center the input range on zero.
+    In centered_input = Clamp(value, in_lo_, in_hi_) - in_midpoint_;
+
+    // Cut away the deadband from the centered input.
+    if (Nabs(centered_input) > -deadband_) {
+      centered_input = 0;
+    } else {
+      centered_input -= SignOf(centered_input) * deadband_;
+    }
+
+    // Scale by output to input ratio. Round away from 0 so that values immediately outside of the
+    // deadband map to 0.
+    const auto centered_output =
+        in_to_out_scaler_.Scale(centered_input, RoundPolicy::kRoundAwayFromZero);
+
+    // Shift range from zero into range.
+    const auto out_value = static_cast<Out>(centered_output + out_midpoint_);
+
+    // Clamp output within range.
+    if (requires_out_clamp_) {
+      return Clamp(out_value, std::get<0>(out_range_), std::get<1>(out_range_));
+    }
+    return out_value;
+  }
+
+ private:
+  // Type used for computation (deduced from promotion).
+  using Intermediate = decltype(std::declval<In>() + std::declval<Out>());
+  static_assert(std::is_integral_v<Intermediate> && std::is_signed_v<Intermediate>,
+                "Only valid for signed integers.");
+
+  constexpr Intermediate in_width() const {
+    MAYS_CHECK(in_lo_ < in_hi_);
+    MAYS_CHECK(deadband_ >= 0);
+    // Input width might overflow Intermediate, so conservatively limit their magnitudes.
+    // NOLINTNEXTLINE(bugprone-assert-side-effect)
+    MAYS_CHECK(Nabs(in_hi_) > std::numeric_limits<Intermediate>::min() / 2);
+    // NOLINTNEXTLINE(bugprone-assert-side-effect)
+    MAYS_CHECK(Nabs(in_lo_) > std::numeric_limits<Intermediate>::min() / 2);
+    MAYS_CHECK(Intermediate{in_hi_} - in_lo_ > 2 * deadband_);
+    return Intermediate{in_hi_} - in_lo_ - 2 * deadband_;
+  }
+
+  constexpr Intermediate out_width() const {
+    // NOLINTNEXTLINE(bugprone-assert-side-effect)
+    MAYS_CHECK(Nabs(std::get<1>(out_range_)) > std::numeric_limits<Intermediate>::min() / 2);
+    // NOLINTNEXTLINE(bugprone-assert-side-effect)
+    MAYS_CHECK(Nabs(std::get<0>(out_range_)) > std::numeric_limits<Intermediate>::min() / 2);
+    return Intermediate{std::get<1>(out_range_)} - std::get<0>(out_range_);
+  }
+
+  [[nodiscard]] constexpr bool requires_out_clamp() const {
+    return (in_width() % 2) || (out_width() % 2);
+  }
+
+  constexpr Scaler<In, Out, In> ComputeScaler(bool invert) {
+    const auto in_width = this->in_width();
+    const auto out_width = NegateIf(this->out_width(), invert);
+    const Intermediate gcd = std::gcd(in_width, out_width);
+    return {static_cast<Out>(out_width / gcd), static_cast<In>(in_width / gcd)};
+  }
+
+  const In in_lo_;
+  const In in_hi_;
+  const In deadband_;
+
+  const In in_midpoint_;
+  const std::tuple<Out, Out> out_range_;  // Sorted output limits.
+  const Out out_midpoint_;
+  const Scaler<In, Out, In> in_to_out_scaler_;
+  const bool requires_out_clamp_;
+};
+
+// Template deduction guides
+template <typename In, typename Out>
+RangeMap(std::initializer_list<In>, std::initializer_list<Out>) -> RangeMap<In, Out>;
+
+template <typename In, typename Out>
+RangeMap(std::initializer_list<In>, std::initializer_list<Out>, In) -> RangeMap<In, Out>;
+
+}  // namespace mays
+
+#endif  // MAYS_RANGE_MAP_H
---
+++ mays/range_map_test.cc
@@ -0,0 +1,85 @@
+// (C) Copyright 2019 Xo Wang <xo@geekshavefeelings.com>
+// SPDX-License-Identifier: Apache-2.0
+// vim: et:sw=2:ts=2:tw=100
+
+#include "range_map.h"
+
+#include <catch2/catch.hpp>
+
+namespace mays {
+namespace {
+
+constexpr int16_t kServoMin = 1000;
+constexpr int16_t kServoCenter = 1500;
+constexpr int16_t kServoMax = 2000;
+
+TEST_CASE("Map value in a range to another range", "[range_map]") {
+  constexpr RangeMap map({-50, 100}, {kServoMin, kServoMax});
+
+  CHECK(kServoMin == map.Map(-50));
+  CHECK(1500 == map.Map(25));
+  CHECK(kServoMax == map.Map(100));
+
+  // Test that the input is clamped against the range limits.
+  CHECK(kServoMin == map.Map(-1000000));
+  CHECK(kServoMax == map.Map(1000000));
+
+  CHECK(1333 == map.Map(0));
+  CHECK(1667 == map.Map(50));
+}
+
+TEST_CASE("Inputs deadband maps to output midpoint", "[range_map]") {
+  constexpr int kDeadband = 10;
+  constexpr RangeMap map({-100, 100}, {kServoMin, kServoMax}, kDeadband);
+
+  // Test center
+  CHECK(kServoCenter == map.Map(0));
+
+  // Test end stops
+  CHECK(kServoMin == map.Map(-100));
+  CHECK(kServoMax == map.Map(100));
+
+  SECTION("Deadband inputs") {
+    const int i = GENERATE(range(-kDeadband, kDeadband + 1));
+    CAPTURE(i);
+    CHECK(kServoCenter == map.Map(i));
+  }
+
+  // Value just outside of deadband
+  CAPTURE(map.Map(11));
+
+  // Leaving the deadband should move the output value.
+  CHECK(kServoCenter < map.Map(11));
+
+  // But the output shouldn't jump to what it would without deadband.
+  constexpr int kNoDeadbandOut = 1555;  // kServoCenter + 11% of output range
+  CHECK(kNoDeadbandOut > map.Map(11));
+}
+
+TEST_CASE("Map to an inverted range", "[range_map]") {
+  // The output range goes high to low.
+  constexpr RangeMap map({-100, 100}, {kServoMax, kServoMin});
+
+  CHECK(kServoMax == map.Map(-100));
+  CHECK(kServoCenter == map.Map(0));
+  CHECK(kServoMin == map.Map(100));
+
+  CHECK(1250 == map.Map(50));
+  CHECK(1750 == map.Map(-50));
+}
+
+TEST_CASE("Map between ranges of various width parities", "[range_map]") {
+  const auto in_lo = GENERATE(-100, -99);
+  const auto in_hi = GENERATE(99, 100);
+  const auto out_a = GENERATE(0, 1);
+  const auto out_b = GENERATE(9, 10);
+  const auto deadband = GENERATE(0, 1);
+  const RangeMap map({in_lo, in_hi}, {out_a, out_b}, deadband);
+
+  CHECK(out_a == map.Map(in_lo));
+  CHECK(out_b == map.Map(in_hi));
+  CHECK(Average(out_a, out_b) == map.Map(Average(in_lo, in_hi)));
+}
+
+}  // namespace
+}  // namespace mays
